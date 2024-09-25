491aca191dcdd1e7117224a3f2188a65a0380edd
26julius <91038107+26julius@users.noreply.github.com>
2024-09-25 16:03:29 +0800

    Add files via upload

---
+++ Src/counter/src/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return(
+        <>
+        <footer>
+        <hr />
+        <p>
+        &copy; {new Date().getFullYear()} My counter Website: julius ceasar Niebla
+        </p>
+        </footer>
+        </>
+    )
+}
+
+
---
+++ Src/counter/src/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return(
+        <>
+        <header>
+        <h1><center> My Counter Website </center></h1>
+        </header>
+        <hr />
+        </>
+
+    )
+}
